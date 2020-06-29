6f6ba190c9cf9bb93879723a70684d10c13527c3
Evgeniy Chekan <hom3chuk@gmail.com>
2020-06-29 12:34:03 +0300

    add current ET

--- assets/js/romcodex-mobs.js
+++ assets/js/romcodex-mobs.js
@@ -4,6 +4,151 @@ let romcodexMobs = {
         name: 'Smokie',
         url: 'https://www.romcodex.com/monster/20001/smokie'
     },
+    toad: {
+        icon: '/info/assets/mobs/toad.png',
+        name: 'Toad',
+        url: 'https://www.romcodex.com/monster/20005/toad'
+    },
+    ghostring: {
+        icon: '/info/assets/mobs/ghostring.png',
+        name: 'Ghostring',
+        url: 'https://www.romcodex.com/monster/20004/ghostring'
+    },
+    gryphon: {
+        icon: '/info/assets/mobs/gryphon.png',
+        name: 'Gryphon',
+        url: 'https://www.romcodex.com/monster/20009/gryphon'
+    },
+    vagabond: {
+        icon: '/info/assets/mobs/vagabond.png',
+        name: 'Vagabond Wolf',
+        url: 'https://www.romcodex.com/monster/20008/vagabond-wolf'
+    },
+    moonlight: {
+        icon: '/info/assets/mobs/moonlight.png',
+        name: 'Moonlight Flower',
+        url: 'https://www.romcodex.com/monster/30013/moonlight-flower'
+    },
+    hyegun: {
+        icon: '/info/assets/mobs/hyegun.png',
+        name: 'Hyegun',
+        url: 'https://www.romcodex.com/monster/20019/hyegun'
+    },
+    anubis: {
+        icon: '/info/assets/mobs/anubis.png',
+        name: 'Anubis',
+        url: 'https://www.romcodex.com/monster/20018/anubis'
+    },
+    mutant: {
+        icon: '/info/assets/mobs/mutant.png',
+        name: 'Mutant Dragon',
+        url: 'https://www.romcodex.com/monster/20012/mutant-dragon'
+    },
+    kaho: {
+        icon: '/info/assets/mobs/kaho.png',
+        name: 'Firelord Kaho',
+        url: 'https://www.romcodex.com/monster/30044/firelord-kaho'
+    },
+    mastering: {
+        icon: '/info/assets/mobs/mastering.png',
+        name: 'Mastering',
+        url: 'https://www.romcodex.com/monster/20017/mastering'
+    },
+    manager: {
+        icon: '/info/assets/mobs/manager.png',
+        name: 'Clock Tower Manager',
+        url: 'https://www.romcodex.com/monster/20025/clock-tower-manager'
+    },
+    raf: {
+        icon: '/info/assets/mobs/raf.png',
+        name: 'Rafflesia',
+        url: 'https://www.romcodex.com/monster/20023/rafflesia'
+    },
+    alice: {
+        icon: '/info/assets/mobs/alice.png',
+        name: 'Alice',
+        url: 'https://www.romcodex.com/monster/20015/alice'
+    },
+    aa: {
+        icon: '/info/assets/mobs/aa.png',
+        name: 'Arc Angeling',
+        url: 'https://www.romcodex.com/monster/30045/arc-angeling'
+    },
+    zel: {
+        icon: '/info/assets/mobs/zel.png',
+        name: 'Zherlthsh',
+        url: 'https://www.romcodex.com/monster/20016/zherlthsh'
+    },
+    garm: {
+        icon: '/info/assets/mobs/garm.png',
+        name: 'Garm',
+        url: 'https://www.romcodex.com/monster/30039/garm'
+    },
+    chepet: {
+        icon: '/info/assets/mobs/chepet.png',
+        name: 'Chepet',
+        url: 'https://www.romcodex.com/monster/20026/chepet'
+    },
+    storm: {
+        icon: '/info/assets/mobs/storm.png',
+        name: 'Stormy Knight',
+        url: 'https://www.romcodex.com/monster/30040/stormy-knight'
+    },
+    di: {
+        icon: '/info/assets/mobs/di.png',
+        name: 'Dark Illusion',
+        url: 'https://www.romcodex.com/monster/20022/dark-illusion'
+    },
+    duke: {
+        icon: '/info/assets/mobs/duke.png',
+        name: 'Owl Duke',
+        url: 'https://www.romcodex.com/monster/20013/owl-duke'
+    },
+    eddga: {
+        icon: '/info/assets/mobs/eddga.png',
+        name: 'Eddga',
+        url: 'https://www.romcodex.com/monster/30011/eddga'
+    },
+    deta: {
+        icon: '/info/assets/mobs/deta.png',
+        name: 'Detarderous',
+        url: 'https://www.romcodex.com/monster/30017/detarderous'
+    },
+    baron: {
+        icon: '/info/assets/mobs/baron.png',
+        name: 'Owl Baron',
+        url: 'https://www.romcodex.com/monster/30018/owl-baron'
+    },
+    orcbaby: {
+        icon: '/info/assets/mobs/orcbaby.png',
+        name: 'Orc Baby',
+        url: 'https://www.romcodex.com/monster/20010/orc-baby'
+    },
+    woodgoblin: {
+        icon: '/info/assets/mobs/woodgoblin.png',
+        name: 'Wood Goblin',
+        url: 'https://www.romcodex.com/monster/20020/wood-goblin'
+    },
+    kobold: {
+        icon: '/info/assets/mobs/kobold.png',
+        name: 'Kobold Leader',
+        url: 'https://www.romcodex.com/monster/30024/kobold-leader'
+    },
+    maya: {
+        icon: '/info/assets/mobs/maya.png',
+        name: 'Maya',
+        url: 'https://www.romcodex.com/monster/30008/maya'
+    },
+    mistress: {
+        icon: '/info/assets/mobs/mistress.png',
+        name: 'Mistress',
+        url: 'https://www.romcodex.com/monster/30007/mistress'
+    },
+    dragonfly: {
+        icon: '/info/assets/mobs/dragonfly.png',
+        name: 'Draoon Fly',
+        url: 'https://www.romcodex.com/monster/20007/dragon-fly'
+    },
     gtb: {
         icon: '/info/assets/mobs/gtb.png',
         name: 'Golden Thief Bug',
