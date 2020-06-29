f969f0fc90a6d06dabf572c6999ed9e703aaeaf9
Evgeniy Chekan <hom3chuk@gmail.com>
2020-06-29 09:54:58 +0300

    add et test page

---
+++ assets/js/romcodex-mobs.js
@@ -0,0 +1,12 @@
+let romcodexMobs = {
+    smokie: {
+        icon: '/info/assets/mobs/smokie.png',
+        name: 'Smokie',
+        url: 'https://www.romcodex.com/monster/20001/smokie'
+    },
+    gtb: {
+        icon: '/info/assets/mobs/gtb.png',
+        name: 'Golden Thief Bug',
+        url: 'https://www.romcodex.com/monster/30002/golden-thief-bug'
+    }
+}
